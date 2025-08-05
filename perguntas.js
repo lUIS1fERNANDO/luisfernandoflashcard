e544bd18b2acffb48a6671c738d3a40caaa02c7e
lUIS1fERNANDO <luis.lima.santos21@escola.pr.gov.br>
2025-08-05 10:48:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Perguntas gerais',
+    'Qual é o maior oceano do mundo?',
+    'O maior oceano do mundo é o Oceano Pacífico.'
+)
+
+criaCartao(
+    'perguntas gerais',
+    'Quem escreveu Dom Quixote?',
+    'O autor de Dom Quixote é Miguel de Cervantes.'
+)
+
+criaCartao(
+    'perguntas gerais',
+    ' Quantos planetas tem no sistema solar?',
+    ' Existem 8 planetas no sistema solar: Mercúrio, Vênus, Terra, Marte, Júpiter, Saturno, Urano e Netuno.'
+)
+
+criaCartao(
+    'perguntas gerais',
+    'Qual é o elemento representado pelo símbolo "O"?',
+    'O símbolo "O" representa o Oxigênio.'
+)
